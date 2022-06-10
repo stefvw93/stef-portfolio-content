cfc7e973f3b6679248ee4c90d6d62d5993e78718
Stef van Wijchen <stefvanwijchen@gmail.com>
2022-06-10 20:04:28 +0200

    add employers

--- keystone.ts
+++ keystone.ts
@@ -17,6 +17,10 @@ import { withAuth, session } from "./auth";
 export default withAuth(
   // Using the config function helps typescript guide you to the available options.
   config({
+    server: {
+      port: 3001,
+    },
+
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
       provider: "sqlite",
--- schema.ts
+++ schema.ts
@@ -2,23 +2,24 @@ import { list } from "@keystone-6/core";
 import {
   text,
   password,
+  checkbox,
+  integer,
   relationship,
-  select,
-  image,
+  virtual,
 } from "@keystone-6/core/fields";
 import { Lists } from ".keystone/types";
 import { document } from "@keystone-6/fields-document";
+import { graphql } from "@graphql-ts/schema";
 
 export const lists: Lists = {
   User: list({
     fields: {
-      name: text({ validation: { isRequired: true } }),
       email: text({
         validation: { isRequired: true },
         isIndexed: "unique",
         isFilterable: true,
       }),
-
+      name: text({ validation: { isRequired: true } }),
       password: password({ validation: { isRequired: true } }),
     },
 
@@ -34,21 +35,51 @@ export const lists: Lists = {
       title: text(),
       slug: text({ isIndexed: "unique", isFilterable: true }),
       content: document({ formatting: true, dividers: true, links: true }),
-      image: image({}),
-      layout: select({
-        options: ["big", "small"],
-      }),
-      categories: select({
-        isIndexed: true,
-        options: ["home", "about", "work", "contact"],
-      }),
-      traits: relationship({ ref: "Trait", many: true }),
+      resume: relationship({ ref: "Resume" }),
     },
   }),
 
-  Trait: list({
+  Resume: list({
     fields: {
-      title: text(),
+      experiences: relationship({ ref: "Experience", many: true }),
+    },
+  }),
+
+  Experience: list({
+    fields: {
+      year: integer(),
+      role: text(),
+      employer: relationship({ ref: "Employer" }),
+      skills: relationship({ ref: "Skill", many: true }),
+      description: virtual({
+        field: graphql.field({
+          type: graphql.String,
+          resolve(item) {
+            return [item.year, item.company].join(" - ");
+          },
+        }),
+      }),
+    },
+
+    ui: {
+      labelField: "description",
+      listView: {
+        initialColumns: ["year", "role"],
+      },
+    },
+  }),
+
+  Employer: list({
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      url: text(),
+      description: text(),
+    },
+  }),
+
+  Skill: list({
+    fields: {
+      name: text(),
     },
   }),
 };
